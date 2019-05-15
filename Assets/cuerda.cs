c0410ef4fb0c53c7bafefd91a8e21685f9dfe5bf
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-05-15 11:53:32 -0600

    Cuerda

---
+++ Assets/cuerda.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cuerda : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<CharacterJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
